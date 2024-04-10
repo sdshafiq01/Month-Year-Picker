8fa033715f5933135d5c98cb9c7d20caf29dfd48
Shafiqul Islam <mia.shafiq4321.com>
2024-04-10 16:48:33 +0600

    update all

---
+++ app/src/main/java/in/thbd/MonthYearPicker/AdsTest.java
@@ -0,0 +1,13 @@
+package in.thbd.MonthYearPicker;
+
+import android.app.Activity;
+
+public class AdsTest {
+
+    Activity act = null;
+
+    public AdsTest(Activity act) {
+        this.act = act;
+
+    }
+}
